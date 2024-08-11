500842ad4783279d878b85e28856130043c700c4
oscar h <oxcarhh@gmail.com>
2024-08-10 22:08:52 -0600

    cambio del JS y funcionamiento en HTML

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
-let inputNumeroDePeatones;
-const totalPeatones =  document.querySelector("[data-input-boton]");
-totalPeatones.addEventListener("click", tiempoTotalVerde);
 
-function tiempoTotalVerde(){
-    inputNumeroDePeatones =  document.querySelector("[data-input-peatones]");
-    valorEnNumero = Number(inputNumeroDePeatones.value);
-    let tiempoEnVerde = 60; /*Equivale a un minuto*/
-    
-    if (valorEnNumero <= 4 && valorEnNumero > 0) {
-        tiempoEnVerde = tiempoEnVerde - 10;
-        let caja = document.querySelector(".caja");
-        caja.classList.add("caja");
-        caja.innerHTML = `El tiempo de espera de ${valorEnNumero} peaton(es) se reduce de 60 segundos a ${tiempoEnVerde} segundos`;
-        inputNumeroDePeatones.value = "";
-    }else if(valorEnNumero > 4){
-        tiempoEnVerde = tiempoEnVerde - 20;
-        let caja = document.querySelector(".caja");
-        caja.classList.add("caja");
-        caja.innerHTML = `El tiempo de espera de ${valorEnNumero} peaton(es) se reduce de 60 segundos a ${tiempoEnVerde} segundos`;
-        inputNumeroDePeatones.value = "";
-    }else if(valorEnNumero <= 0){
-        let caja = document.querySelector(".caja");
-        caja.classList.add("caja");
-        caja.innerHTML = `Datos no validos, ingresa un número mayor a 0`;
-        inputNumeroDePeatones.value = "";
-    }
+const contenedorParrafo = document.querySelector("[data-parrafo]");
+const parrafo = document.createElement("p");
+
+function climaBueno(){
+    parrafo.textContent = "Calidad del aíre satisfactoria. Sin riesgo.";
+    contenedorParrafo.appendChild(parrafo);
+    mad = document.getElementById("miImg");
+    mad.src = "/img/bueno.png";
+}
+
+function climaRegular(){
+    parrafo.textContent = "La calidad del aíre es aceptable.";
+    contenedorParrafo.appendChild(parrafo);
+    mad = document.getElementById("miImg");
+    mad.src = "/img/regular.png";
+}
+
+function climaMalo(){
+    parrafo.textContent = "La calidad del aíre es mala, dañina a grupos sensibles.";
+    contenedorParrafo.appendChild(parrafo);
+    mad = document.getElementById("miImg");
+    mad.src = "/img/malo.png";
+}
+
+function climaMuyMalo(){
+    parrafo.textContent = "La calidad del aíre es muy mala, dañina a la salud.";
+    contenedorParrafo.appendChild(parrafo);
+    mad = document.getElementById("miImg");
+    mad.src = "/img/muyMalo.png";
+}
+
+function climaExtremo(){
+    parrafo.textContent = "La calidad del aíre es extremadamente mala, muy dañina a la salud.";
+    contenedorParrafo.appendChild(parrafo);
+    mad = document.getElementById("miImg");
+    mad.src = "/img/extremadamente.png";
 }
