7527bf0d93d07cea1acac2edb937321b43e5225c
oscar h <oxcarhh@gmail.com>
2024-08-11 12:26:33 -0600

    cambioen JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const contenedorParrafo = document.querySelector("[data-parrafo]");
 const parrafo = document.createElement("p");
 
@@ -6,33 +5,33 @@ function climaBueno(){
     parrafo.textContent = "Calidad del aíre satisfactoria. Sin riesgo.";
     contenedorParrafo.appendChild(parrafo);
     mad = document.getElementById("miImg");
-    mad.src = "/img/bueno.png";
+    mad.src = "img/bueno.png";
 }
 
 function climaRegular(){
     parrafo.textContent = "La calidad del aíre es aceptable.";
     contenedorParrafo.appendChild(parrafo);
     mad = document.getElementById("miImg");
-    mad.src = "/img/regular.png";
+    mad.src = "img/regular.png";
 }
 
 function climaMalo(){
     parrafo.textContent = "La calidad del aíre es mala, dañina a grupos sensibles.";
     contenedorParrafo.appendChild(parrafo);
     mad = document.getElementById("miImg");
-    mad.src = "/img/malo.png";
+    mad.src = "img/malo.png";
 }
 
 function climaMuyMalo(){
     parrafo.textContent = "La calidad del aíre es muy mala, dañina a la salud.";
     contenedorParrafo.appendChild(parrafo);
     mad = document.getElementById("miImg");
-    mad.src = "/img/muyMalo.png";
+    mad.src = "img/muyMalo.png";
 }
 
 function climaExtremo(){
     parrafo.textContent = "La calidad del aíre es extremadamente mala, muy dañina a la salud.";
     contenedorParrafo.appendChild(parrafo);
     mad = document.getElementById("miImg");
-    mad.src = "/img/extremadamente.png";
+    mad.src = "img/extremadamente.png";
 }
